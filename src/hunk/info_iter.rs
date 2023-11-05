96eea446c20a8fadc588b0a0fc8df1afdfd5bbf4
Yuri Lifanov <yurilifanov@gmail.com>
2023-11-05 14:39:58 +0100

    tweak InfoIter API

--- src/hunk/info_iter.rs
+++ src/hunk/info_iter.rs
@@ -1,14 +1,14 @@
+use core::cmp::Ordering;
+use core::iter::Filter;
 use std::iter::Peekable;
 
-#[derive(Debug, PartialEq)]
-pub struct Info<'a> {
-    pub line: &'a str,
-    pub num: usize,
-}
+type Info<'a> = (Option<&'a str>, Option<&'a str>, usize);
 
 pub struct InfoIter<'a, T: Iterator<Item = &'a str> + Clone> {
     after: Peekable<LineIter<'a, T>>,
     before: Peekable<LineIter<'a, T>>,
+    num_after: usize,
+    num_before: usize,
 }
 
 impl<'a, T: Iterator<Item = &'a str> + Clone> InfoIter<'a, T> {
@@ -17,33 +17,60 @@ impl<'a, T: Iterator<Item = &'a str> + Clone> InfoIter<'a, T> {
             after: LineIter::<'a> {
                 iter: iter.clone(),
                 prefix: '+',
-                num: header[2],
             }
             .peekable(),
             before: LineIter::<'a> {
                 iter: iter,
                 prefix: '-',
-                num: header[0],
             }
             .peekable(),
+            num_after: header[2],
+            num_before: header[0],
         }
     }
 }
 
+fn post_increment(reference: &mut usize) -> usize {
+    let prev: usize = *reference;
+    *reference += 1;
+    prev
+}
+
 impl<'a, T: Iterator<Item = &'a str> + Clone> Iterator for InfoIter<'a, T> {
-    type Item = (Option<Info<'a>>, Option<Info<'a>>);
+    type Item = Info<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
         match (self.after.peek(), self.before.peek()) {
-            (Some(a), Some(b)) => {
-                match (a.line.starts_with(' '), b.line.starts_with(' ')) {
-                    (true, false) => Some((None, self.before.next())),
-                    (false, true) => Some((self.after.next(), None)),
-                    _ => Some((self.after.next(), self.before.next())),
+            (Some(a), Some(b)) => match self.num_after.cmp(&self.num_before) {
+                Ordering::Greater => Some((
+                    None,
+                    self.before.next(),
+                    post_increment(&mut self.num_before),
+                )),
+                Ordering::Less => Some((
+                    self.after.next(),
+                    None,
+                    post_increment(&mut self.num_after),
+                )),
+                _ => {
+                    self.num_before += 1;
+                    Some((
+                        self.after.next(),
+                        self.before.next(),
+                        post_increment(&mut self.num_after),
+                    ))
                 }
-            }
-            (Some(_), None) => Some((self.after.next(), None)),
-            (None, Some(_)) => Some((None, self.before.next())),
+            },
+            (Some(_), None) => Some((
+                self.after.next(),
+                None,
+                post_increment(&mut self.num_after),
+            )),
+            (None, Some(_)) => Some((
+                None,
+                self.before.next(),
+                post_increment(&mut self.num_after),
+            )),
             _ => None,
         }
     }
@@ -52,19 +79,16 @@ impl<'a, T: Iterator<Item = &'a str> + Clone> Iterator for InfoIter<'a, T> {
 struct LineIter<'a, T: Iterator<Item = &'a str> + Clone> {
     iter: T,
     prefix: char,
-    num: usize,
 }
 
 impl<'a, T: Iterator<Item = &'a str> + Clone> Iterator for LineIter<'a, T> {
-    type Item = Info<'a>;
+    type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             let line = self.iter.next()?;
             if line.starts_with([' ', self.prefix]) {
-                let num = self.num;
-                self.num += 1;
-                return Some(Info { line, num });
+                return Some(line);
             }
         }
     }
@@ -74,32 +98,48 @@ impl<'a, T: Iterator<Item = &'a str> + Clone> Iterator for LineIter<'a, T> {
 mod tests {
     use crate::hunk::info_iter::{Info, InfoIter};
 
-    #[test]
-    fn test_info_iter() {
-        let string = "+ -+ +- -";
+    fn split(line: &str) -> impl Iterator<Item = &str> + Clone {
+        line.char_indices()
+            .zip(line.char_indices().skip(1).chain(Some((line.len(), ' '))))
+            .map(move |((i, _), (j, _))| &line[i..j])
+    }
 
-        let data: Vec<String> =
-            string.chars().map(|c| c.to_string()).collect();
-
-        fn info<'a>(s: &'a str, n: usize) -> Option<Info<'a>> {
-            Some(Info { line: s, num: n })
-        }
-
-        let header: [usize; 4] = [1, 0, 1, 0];
-
-        let expected = [
-            (info("+", 1), None),
-            (info(" ", 2), info(" ", 1)),
-            (info("+", 3), info("-", 2)),
-            (info(" ", 4), info(" ", 3)),
-            (info("+", 5), info("-", 4)),
-            (info(" ", 6), info(" ", 5)),
-            (None, info("-", 6)),
-        ];
-
-        let actual = InfoIter::new(&header, data.iter().map(|s| s.as_str()));
+    fn test<'a, T: Iterator<Item = &'a str> + Clone>(
+        header: [usize; 4],
+        lines: T,
+        expected: Vec<Info>,
+    ) {
+        let actual = InfoIter::new(&header, lines);
         for (act, exp) in actual.zip(expected.iter()) {
             assert_eq!(act, *exp);
         }
+    }
+
+    #[test]
+    fn case_1() {
+        let header: [usize; 4] = [1, 0, 1, 0];
+        let line = "+ -+ +- -";
+        let expected: Vec<Info> = vec![
+            (Some("+"), Some(" "), 1),
+            (Some(" "), Some("-"), 2),
+            (Some("+"), Some(" "), 3),
+            (Some(" "), Some("-"), 4),
+            (Some("+"), Some(" "), 5),
+            (Some(" "), Some("-"), 6),
+        ];
+        test(header, split(line), expected);
+    }
+
+    #[test]
+    fn case_2() {
+        let header: [usize; 4] = [3, 0, 1, 0];
+        let line = "+++- ";
+        let expected: Vec<Info> = vec![
+            (Some("+"), None, 1),
+            (Some("+"), None, 2),
+            (Some("+"), Some("-"), 3),
+            (Some(" "), Some(" "), 4),
+        ];
+        test(header, split(line), expected);
     }
 }
